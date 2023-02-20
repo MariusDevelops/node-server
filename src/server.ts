75ca6ab43e52ed829b86dee7d86d634f546171c7
MariusDevelops <brazasmarius@gmail.com>
2023-02-20 22:21:57 +0200

    feat: basic api router

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
 
+// setup
 dotenv.config();
 
 const { SERVER_PORT, SERVER_DOMAIN } = process.env;
@@ -13,9 +14,34 @@ if (SERVER_PORT === undefined || SERVER_DOMAIN === undefined) {
 
 const server = express();
 
+// Middlewares
 server.use(morgan('tiny'));
 server.use(express.static('public'));
+server.use(express.json());
 
+// API Router
+const houses = [
+  { id: 1, title: 'House 1' },
+  { id: 2, title: 'House 2' },
+  { id: 3, title: 'House 3' },
+  { id: 4, title: 'House 4' },
+];
+
+const apiRouter = express.Router();
+server.use('/api', apiRouter);
+
+apiRouter.get('/houses', (req, res) => {
+  res.status(200).json(houses);
+});
+
+apiRouter.post('/houses', (req, res) => {
+  const { body } = req;
+  const newHouse = { id: 5, title: body.title };
+  houses.push(newHouse);
+  res.status(201).json(newHouse);
+});
+
+// Server init
 server.listen(SERVER_PORT, () => {
   console.log(`server is running on: http://${SERVER_DOMAIN}:${SERVER_PORT}`);
 });
