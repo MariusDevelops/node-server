ce8cbf72dfea421d7a5e8c9d4fa656c60175cb76
MariusDevelops <brazasmarius@gmail.com>
2023-02-21 23:19:01 +0200

    feat: created router

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,7 @@
-/* eslint-disable no-console */
-import * as dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
-
-// setup
-dotenv.config();
-
-const { SERVER_PORT, SERVER_DOMAIN } = process.env;
-
-if (SERVER_PORT === undefined || SERVER_DOMAIN === undefined) {
-  throw new Error("Please define constants in '.env' file");
-}
+import config from './config';
+import housesRouter from './routers/houses-router';
 
 const server = express();
 
@@ -18,30 +9,8 @@ const server = express();
 server.use(morgan('tiny'));
 server.use(express.static('public'));
 server.use(express.json());
+server.use('/api/houses', housesRouter);
 
-// API Router
-const houses = [
-  { id: 1, title: 'House 1' },
-  { id: 2, title: 'House 2' },
-  { id: 3, title: 'House 3' },
-  { id: 4, title: 'House 4' },
-];
-
-const apiRouter = express.Router();
-server.use('/api', apiRouter);
-
-apiRouter.get('/houses', (req, res) => {
-  res.status(200).json(houses);
-});
-
-apiRouter.post('/houses', (req, res) => {
-  const { body } = req;
-  const newHouse = { id: 5, title: body.title };
-  houses.push(newHouse);
-  res.status(201).json(newHouse);
-});
-
-// Server init
-server.listen(SERVER_PORT, () => {
-  console.log(`server is running on: http://${SERVER_DOMAIN}:${SERVER_PORT}`);
+server.listen(config.server.port, () => {
+  console.log(`server is running on: http://${config.server.domain}:${config.server.port}`);
 });
