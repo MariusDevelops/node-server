98116ab4096ff73bdaf3f0da6631f2673b009017
MariusDevelops <brazasmarius@gmail.com>
2023-02-23 11:24:59 +0200

    feat: connected to mySql

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,17 @@ import express from 'express';
 import morgan from 'morgan';
 import config from './config';
 import housesController from './controllers/houses-controller';
+import { connectMySql } from './services/my-sql';
 
 const server = express();
 
-// Middlewares
 server.use(morgan('tiny'));
 server.use(express.static('public'));
 server.use(express.json());
 server.use('/api/houses/', housesController);
 
-server.listen(config.server.port, () => {
-  console.log(`server is running on: http://${config.server.domain}:${config.server.port}`);
+connectMySql(() => {
+  server.listen(config.server.port, () => {
+    console.log(`server is running on: http://${config.server.domain}:${config.server.port}`);
+  });
 });
