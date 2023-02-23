9f267276f51b60186d735e45d78a4aaf89b05014
MariusDevelops <brazasmarius@gmail.com>
2023-02-23 09:26:53 +0200

    feat: reused schemas

--- src/controllers/houses-controller/validation-schemas/partial-house-data-validation-schema.ts
+++ src/controllers/houses-controller/validation-schemas/partial-house-data-validation-schema.ts
@@ -1,41 +1,17 @@
 import * as yup from 'yup';
 import { PartialHouseData } from '../types';
+import imagesSchema from './images-schema';
+import locationSchema from './location-schema';
+import priceSchema from './price-schema';
+import ratingSchema from './rating-schema';
+import titleSchema from './title-schema';
 
 const partialHouseDataValidationSchema: yup.ObjectSchema<PartialHouseData> = yup.object({
-  title: yup.string()
-    .min(2, 'title must have at least 2 symbols')
-    .max(32, 'title can\'t have more than 32 symbols'),
-
-  price: yup.number()
-    .positive('price must be positive')
-    .test(
-      'isPrice',
-      'icorrect price format',
-      (val) => {
-        if (val === undefined) return true;
-        return Number(val.toFixed(2)) === val;
-      },
-    ),
-
-  rating: yup.number()
-    .positive('rating must be positive')
-    .min(1, 'rating must be at least 1')
-    .max(5, 'rating cant\'t be more than 5'),
-
-  images: yup.array(yup.string().required())
-    .min(1, 'images must have at least one image'),
-
-  location: yup
-    .object({
-      country: yup.string()
-        .required('location.title is required')
-        .min(2, 'location.title must have at least 2 symbols')
-        .max(32, 'location.title can\'t have more than 32 symbols'),
-      city: yup.string()
-        .required('location.city is required')
-        .min(2, 'location.city must have at least 2 symbols')
-        .max(32, 'location.city can\'t have more than 32 symbols'),
-    }),
+  title: titleSchema,
+  price: priceSchema(),
+  rating: ratingSchema,
+  images: imagesSchema,
+  location: locationSchema,
 }).strict(true);
 
 export default partialHouseDataValidationSchema;
