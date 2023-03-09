d40af12e6fb8842d3d21e9cd53a6def5340d4c9d
MariusDevelops <brazasmarius@gmail.com>
2023-03-09 17:59:20 +0200

    feat: add create user

--- src/auth/register.ts
+++ src/auth/register.ts
@@ -2,6 +2,7 @@ import { RequestHandler } from 'express';
 import ErrorService from 'services/error-service';
 import { AuthSuccessResponse, RegistrationData } from './types';
 import registrationDataValidationSchema from './validation-schemas/registration-data-validation-schema';
+import UserModel from './model';
 
 export const register: RequestHandler<
   {},
@@ -14,7 +15,31 @@ export const register: RequestHandler<
       abortEarly: false,
     });
 
-    res.status(200).json(registrationData as unknown as AuthSuccessResponse);
+    // TODO: patikrinti ar paštas yra lasivas - galimas.
+
+    const user = await UserModel.createUser({
+      email: registrationData.email,
+      password: registrationData.password, // TODO: užšifruoti
+      name: registrationData.name,
+      surname: registrationData.surname,
+    });
+
+    /*
+      // 1. Sukurti vartotoją
+      //   * pradžioje išsibandome MySql Workbench'e
+      //   * sukuriame modelio metodą - createUser
+      2. Sukurti tokeną
+        * sukurti atskirą service'ą kurti ir dekoduoti token'ui
+        * aprašyti jame metodą - createToken
+        * panaudoti jį tam, kad sukurti token'ą register/login metoduose
+      3. išsiųsti duomenis - AuthSuccessResponse
+        * sukuriame helper'į - createAuthSuccessResponse(user)
+          * token'o sukūrimas perkelimas į funkcijos vidų
+        * pritaikome createAuthSuccessResponse register/login metoduose
+        * siunčiame duomenis naudodami <res> objektą
+    */
+
+    res.status(200).json(user as unknown as AuthSuccessResponse);
   } catch (err) {
     const [status, errorResponse] = ErrorService.handleError(err);
     res.status(status).json(errorResponse);
