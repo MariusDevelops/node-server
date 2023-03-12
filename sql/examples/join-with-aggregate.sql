ba7287dd3025bc1b9e3bfaa29005407939c558cf
MariusDevelops <brazasmarius@gmail.com>
2023-03-13 00:00:12 +0200

    feat: join-with-aggregate update

--- sql/examples/join-with-aggregate.sql
+++ sql/examples/join-with-aggregate.sql
@@ -22,3 +22,28 @@ ON h.locationId = l.id
 LEFT JOIN users as u
 ON u.id = h.ownerId
 GROUP BY h.id;
+
+-- SELECT 
+-- 	h.id, 
+--     h.title, 
+--     JSON_OBJECT(
+-- 		'material', l.material,
+--         'sizes', l.sizes
+-- 	) as details, 
+--   h.price,
+--   h.rating,
+--   IF(COUNT(i.id) = 0, JSON_ARRAY(), JSON_ARRAYAGG(i.src)) as images,
+--   JSON_OBJECT(
+-- 		'name', u.name,
+-- 		'surname', u.surname,
+-- 		'email', u.email,
+-- 		'mobile', u.mobile
+-- 	) as owner
+-- FROM images as i
+-- LEFT JOIN products as h
+-- ON i.productId = h.id
+-- LEFT JOIN  details as l
+-- ON h.detailId = l.id
+-- LEFT JOIN users as u
+-- ON u.id = h.ownerId
+-- GROUP BY h.id;
